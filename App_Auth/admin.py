ed1cd2a8376fe7223e56a1c574f461b265ffa4e5
gbolahan <thegbolahanalaba@gmail.com>
2024-10-30 14:39:17 +0100

    New App added

--- App_Auth/admin.py
+++ App_Auth/admin.py
@@ -8,32 +8,3 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'phone', 'email', 'role', 'sector', 'auth_code']
     list_filter = ['first_name']
     search_fields = ['first_name']
-
-
-@admin.register(Team)
-class TeamAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created_by', 'created_at', 'updated_at']
-    list_filter = ['name']
-    search_fields = ['name']
-
-
-@admin.register(Sector)
-class SectorAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created_by', 'created_at', 'updated_at']
-    list_filter = ['name']
-    search_fields = ['name']
-
-@admin.register(AuthCode)
-class AuthCodeAdmin(admin.ModelAdmin):
-    list_display = ['auth_code', 'created_at', 'updated_at']
-    list_filter = ['auth_code']
-    search_fields = ['auth_code']
-
-
-
-
-
-# admin.site.register(User, UserAdmin)
-# admin.site.register(Roles, RoleAdmin)
-# admin.site.register(Sectors, SectorAdmin)
-# admin.site.register(AuthCode, AuthAdmin)
--- App_Users/admin.py
+++ App_Users/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from . models import *
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_by', 'created_at', 'updated_at']
+    list_filter = ['name']
+    search_fields = ['name']
+
+
+@admin.register(Sector)
+class SectorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_by', 'created_at', 'updated_at']
+    list_filter = ['name']
+    search_fields = ['name']
+
+@admin.register(AuthCode)
+class AuthCodeAdmin(admin.ModelAdmin):
+    list_display = ['auth_code', 'created_at', 'updated_at']
+    list_filter = ['auth_code']
+    search_fields = ['auth_code']
+
+
+
+
+
+# admin.site.register(User, UserAdmin)
+# admin.site.register(Roles, RoleAdmin)
+# admin.site.register(Sectors, SectorAdmin)
+# admin.site.register(AuthCode, AuthAdmin)
